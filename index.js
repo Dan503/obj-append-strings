00576cdce6a534a5637741d5794d64ad38df71b7
Daniel Tonon <daniel.tonon@adelphi.digital>
2017-02-04 00:39:43 +1100

    Moved appendee undefined check into recursive function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function appendStrings(appendee, appender, position, key){
+	appendee = typeof appendee !== 'undefined' ? appendee : {};
+
 	if (typeof appendee[key] === 'object' && !Array.isArray(appendee[key])){
 		for (var subKey in appendee[key]){
 			appendStrings(appendee[key], appender[key], position, subKey);
@@ -15,7 +17,6 @@ function appendStrings(appendee, appender, position, key){
 
 module.exports = function(appendee, appender, position){
 	position = typeof position !== 'undefined' ? position : 'after';
-	appendee = typeof appendee !== 'undefined' ? appendee : {};
 
 	for (var key in appender){
 		appendStrings(appendee, appender, position, key);
